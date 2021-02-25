9eda1143affcbbff3d1e14ad83ef8da557e91054
Thibault Ballier <me@tbmc.fr>
2021-02-25 19:46:39 +0100

    Read data

--- read_data.py
+++ read_data.py
@@ -1,12 +1,25 @@
 from typing import Tuple, List, Dict
 from collections import defaultdict
+from dataclasses import dataclass
 
 
 ROAD_DICT_TYPE = Dict[str, Tuple[int, int, int]]
 
 
-def read_data(file_path: str)\
-        -> Tuple[Tuple[int, int, int, int, int], ROAD_DICT_TYPE, Dict[int, List[str]], List[List[str]]]:
+@dataclass
+class DataContainer:
+    duration: int
+    intersection_number: int
+    street_number: int
+    car_number: int
+    bonus_points: int
+
+    road_dict: ROAD_DICT_TYPE
+    intersection_dict: Dict[int, Tuple[List[str], List[str]]]
+    car_list: List[List[str]]
+
+
+def read_data(file_path: str) -> DataContainer:
     with open(file_path) as f:
         content = f.read()
 
@@ -16,8 +29,9 @@ def read_data(file_path: str)\
     cars = content[S + 1:]
 
     road_dict: ROAD_DICT_TYPE = {}  # key, (start, end, time)
-    intersection_dict: Dict[int, List[str]] = defaultdict(list)  # id, [connected roads]
-    cards_list: List[List[str]] = []  # roads it wants to travel through
+    # id, # (list of input road, list of output roads)
+    intersection_dict: Dict[int, Tuple[List[str], List[str]]] = defaultdict(lambda: ([], []))
+    car_list: List[List[str]] = []  # roads it wants to travel through
 
     for road in roads:
         start, end, name, time = road.split()
@@ -26,12 +40,13 @@ def read_data(file_path: str)\
         time = int(time)
 
         road_dict[name] = start, end, time
-        intersection_dict[start].append(name)
+        intersection_dict[start][0].append(name)
+        intersection_dict[end][1].append(name)
 
     for car in cars:
-        cards_list.append(car.split()[1:])
+        car_list.append(car.split()[1:])
 
-    return (D, I, S, V, F), road_dict, intersection_dict, cards_list
+    return DataContainer(D, I, S, V, F, road_dict, intersection_dict, car_list)
 
 
 
